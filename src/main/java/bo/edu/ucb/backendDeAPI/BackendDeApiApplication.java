ee46eccf50973e814bcdb6d9f18c23982d450e19
AlanocaBMLW <solaror50@gmail.com>
2023-09-04 20:02:32 -0400

    primer commit

---
+++ src/main/java/bo/edu/ucb/backendDeAPI/BackendDeApiApplication.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.backendDeAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bo/edu/ucb/backendDeAPI/BackendDeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.backendDeAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
